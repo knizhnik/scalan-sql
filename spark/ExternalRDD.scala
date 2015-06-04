a6d3a47dab2fcd60070a5db380c8d679d73d4e7e
Konstantin Knizhnik <knizhnik@garret.ru>
2015-06-04 15:06:17 +0300

    Add ExternalRDD

--- spark/ExternalRDD.scala
+++ spark/ExternalRDD.scala
@@ -1,17 +1,19 @@
+package com.huawei.hispark
+
 import org.apache.spark.rdd
 import org.apache.spark.sql.catalyst.expressions.Row
 
-class ExternalRDD(input: SchemaRDD) extends RDD[Int]
+class ExternalRDD(input: SchemaRDD, push: Boolean) extends RDD[Int]
 {
     System.loadLibrary("nativerdd")
 
     def compute(split: Partition, context: TaskContext): Iterator[Int] = {
       val i = input.compute(split, context)
-      val sum = native_sum(i.toArray)
+      val sum = if push nativeSumPush(i.toArray) else nativeSumPull(i)
       Seq(sum).iterator
       /*
       var jobResult = 0
-      val sumPartition = (iter: Iterator[Row]) => native_sum(i.toArray)
+      val sumPartition = (iter: Iterator[Row]) => nativeSum(i.toArray)
       val mergeResults = (index: Int, taskResult: Int) => jobResult = jobResult + taskResult
       sc.runJob(this, sumPartition, mergeResults)
       */
@@ -21,5 +23,6 @@ class ExternalRDD(input: SchemaRDD) extends RDD[Int]
     protected def getPartitions: Array[Partition] = input.getPartitions
     protected def getDependencies: Seq[Dependency[_]] = input.getDependencies
 
-    @native def native_sum(arr: Array[Row])
+    @native def nativeSumPush(arr: Array[Row]):Int
+    @native def nativeSumPull(iter: Iterator[Row]):Int
 }
