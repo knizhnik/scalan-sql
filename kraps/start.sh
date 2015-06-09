900e6b1549c369631511bb281ff528104c6e4c33
Konstantin Knizhnik <knizhnik@garret.ru>
2015-06-09 17:02:01 +0300

    Add hdfs support

--- kraps/start.sh
+++ kraps/start.sh
@@ -4,10 +4,11 @@ n_nodes=16
 nodes=""
 for ((i=0;i<n_nodes;i++))
 do
-    nodes="$nodes ${hosts[i % n_hosts]}:600$i"
+    nodes="$nodes ${hosts[i % n_hosts]}:500$i"
 done
 for ((i=0;i<n_nodes;i++))
 do
-    ssh "${hosts[i % n_hosts]}" "export LD_LIBRARY_PATH=. ; kraps/tpch $i $n_nodes $nodes" > node$i.log 2>&1 &
+#      ssh "${hosts[i % n_hosts]}" "cd /srv/remote/all-common/tpch/data ; ulimit -c unlimited ; export LD_LIBRARY_PATH=. ; ./tpch -cache -tmp ~ -inmem-threshold 10000 $i $n_nodes $nodes" > node$i.log 2>&1 &
+	ssh "${hosts[i % n_hosts]}" "cd /srv/remote/all-common/tpch/data ; ulimit -c unlimited ; export LD_LIBRARY_PATH=. ; ./tpch -dir hdfs://strong:9000 -format parquet -tmp ~ -inmem-threshold 100000000 $i $n_nodes $nodes" > node$i.log 2>&1 &
 done
 wait
