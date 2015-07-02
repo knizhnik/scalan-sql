7eac71238eddbd11131d9fe83a795adc46cd21ef
Konstantin Knizhnik <knizhnik@garret.ru>
2015-07-02 13:10:54 +0300

    Update CustomRDDTest

--- spark/CustomRDDTest.scala
+++ spark/CustomRDDTest.scala
@@ -5,20 +5,20 @@ import org.apache.spark.sql.catalyst.expressions.Row
 
 class CustomRDD(sc: SparkContext, input: RDD[Row]) extends RDD[Int](sc, input.dependencies) {
     def compute(split: Partition, context: TaskContext): Iterator[Int] = {
-      val nodes = sc.getConf.getExecutorMemoryStatus.keys
+      val nodes = sc.getExecutorMemoryStatus.size
       val executor = sc.getConf.get("spark.executor.id")
       val driver = sc.getConf.get("spark.driver.host")
-      println(s"executor=${executor} driver=${driver}")
+      println(s"executor=${executor} driver=${driver} nodes=${nodes}")
       val i = input.compute(split, context)
       var sum = 0
-      while (i.hasNext) sum = sum + i.next
+      while (i.hasNext) sum = sum + i.next.getInt(0)
       Seq(sum).iterator
     }      
 
     protected def getPartitions: Array[Partition] = input.partitions
 }
 
-object NativeTest
+object CustomRDDTest
 {
   def now: Long = java.lang.System.currentTimeMillis()
 
@@ -33,9 +33,10 @@ object NativeTest
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
-    val data_dir = "hdfs://strong:9212/"
+    val data_dir = "hdfs://strong:9121/"
     val nExecutors = sc.getExecutorMemoryStatus.size - 1
-    val lineitem = sqlContext.parquetFile(data_dir + "Lineitem.parquet").rdd.coalesce(nExecutors)
+    println(s"Executors=${nExecutors}"); 
+    val lineitem = sqlContext.parquetFile(data_dir + "Supplier.parquet").rdd.coalesce(nExecutors)
 
     exec(new CustomRDD(sc, lineitem))
   }
