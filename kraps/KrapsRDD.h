7a066743fb159b83369adc409fcd2155ff1f7735
Konstantin Knizhnik <knizhnik@garret.ru>
2015-10-16 19:06:11 +0300

    Update KrapsRDD

--- kraps/KrapsRDD.h
+++ kraps/KrapsRDD.h
@@ -4,28 +4,35 @@
 class KrapsIterator
 {
   public:
-    static ThreadLocal<JNIEnv> env;
-
     virtual void* next() = 0;
     virtual ~KrapsIterator() {}
 };
 
+
+struct JavaContext 
+{
+    JNIEnv* env;
+    jobjectArray inputs;
+
+    JavaContext(JNIEnv* e, jobjectArray i) : env(e), inputs(i) {}
+};
+
 template<class T>
-class SparkRDD : RDD<T>
+class SparkRDD : public RDD<T>
 {
   public:
     bool next(T& row)
     {
-        JNIEnv* env = KrapsIterator::env.get();
-        return env->CallBooleanMethod(iterator, nextRow, (jlong)(size_t)&row);
+        JavaContext* ctx = (JavaContext*)Cluster::instance->userData;
+        return ctx->env->CallBooleanMethod(ctx->env->GetObjectArrayElement(ctx->inputs, inputNo), nextRow, (jlong)(size_t)&row);
     }
-    SparkRDD(JNIEnv* env, jobject iter) : iterator(iter)
+    SparkRDD(JNIEnv* env, jint no) : inputNo(no)
     {
-        jclass rowIteratorClass = (jclass)env->FindClass("RowIterator");
+        jclass rowIteratorClass = (jclass)env->FindClass("kraps/RowIterator");
         nextRow = env->GetMethodID(rowIteratorClass, "next", "(J)Z");
     } 
     
   private:
     jmethodID nextRow;
-    jobject iterator;
+    jint inputNo;
 };
